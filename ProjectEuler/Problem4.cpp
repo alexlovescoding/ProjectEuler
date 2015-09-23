5b65f343d5b4b7374938122a9787012c68093d43
Alex Johnson <alex_johnson24@aol.com>
2015-09-23 13:24:18 -0400

    init

---
+++ ProjectEuler/Problem4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverse_string(string s) {
+	string result = "";
+	for (int i = s.length() - 1; i >= 0; i--) {
+		result = result + s.at(i);
+	}
+	return result;
+}
+
+int main()
+{
+	int first = 999;
+	int second = 999;
+	int largest = 0;
+	string result = "";
+	int endLoop = first-100;
+	while (first >= endLoop) {
+		int product = first*second;
+		string s = to_string(product);
+		string r = reverse_string(s);
+		if (s.compare(r) == 0 && product > largest) {
+			largest = product;
+			result = to_string(first) + "X" + to_string(second);
+		}
+
+		if (second == endLoop) {
+			second = first - 1;
+			first--;
+		}
+		else {
+			second--;
+		}
+
+		if (first < endLoop && largest == 0) {
+			endLoop = endLoop + 100;
+		}
+	}
+
+	cout << largest << "\n" << result;
+}
